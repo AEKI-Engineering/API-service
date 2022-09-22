04e2a17db619aef0f3c701da9628cd6fffc2d9aa
macieopi <42119652+Macieopi@users.noreply.github.com>
2022-09-22 17:48:10 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,9 @@
 from fastapi import FastAPI
+import uvicorn
+import os
+
+# load environment variables
+port = os.environ["PORT"]
 
 app = FastAPI()
 
@@ -6,3 +11,6 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World!"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
